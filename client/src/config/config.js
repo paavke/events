af211a4341d4ab1ad8b0be9316ff2b789a59a18d
Danilo Pavicevic <paavke@users.noreply.github.com>
2026-06-14 13:22:50 +0200

    fix(client): add react-icons and env-based config

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,9 @@
-// src/config.js
 const config = {
-    baseURL: 'http://localhost:8080',
-    realm: 'danilo_eventure',
-    clientId: 'eventure-app',
-    apikey: 'dd4c8216-cfb7-4629-aad3-bb3da1a4c4ec'
+    baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080',
+    authBaseURL: process.env.REACT_APP_AUTH_BASE_URL || 'http://localhost:8080',
+    realm: process.env.REACT_APP_KEYCLOAK_REALM || 'danilo_eventure',
+    clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID || 'eventure-app',
+    apikey: process.env.REACT_APP_APIMAN_API_KEY || 'dd4c8216-cfb7-4629-aad3-bb3da1a4c4ec',
 };
 
 export default config;
